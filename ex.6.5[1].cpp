bbf6c5a1d5df620fd279509ecf2e506d48553fde
Muhammad Taha <127325759+MuhammadTaha01@users.noreply.github.com>
2023-03-20 21:09:06 +0500

    Add files via upload

---
+++ ex.6.5[1].cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,c;
+    c=1;
+    cout<<"Enter a number: ";
+    cin>>n;
+
+    while(c<=10)
+    {
+        cout<<n<<" * "<<c<<endl;
+        c++;
+    }
+    return 0;
+}
